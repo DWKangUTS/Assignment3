59ec9142c684ac45539ea868f9695878303f89f8
DWKangUTS <rkdekdnjs199@naver.com>
2022-09-24 05:10:56 +1000

    Animation and Animator

---
+++ Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    
+}
+
+// if (Input.GetKeyDown(KeyCode.D))
+//animator.SetBool("Right", true);
